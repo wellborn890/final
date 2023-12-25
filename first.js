400c1bc091bfbea171cc5d89d6e3de48837905c1
wellborn <143796045+wellborn890@users.noreply.github.com>
2023-12-25 23:55:29 +0330

    Update first.js

--- first.js
+++ first.js
@@ -71,7 +71,7 @@
         if (number >= 1000000) {
             return (number / 1000000).toFixed(1) + 'M';
         } else if (number >= 1000) {
-            return (number / 1000).toFixed(1) + 'K';
+            return (number / 1000).toFixd(1) + 'K';
         } else {
             return number.toString();
         }
