cdd87f5d869bb1e3eb471a0400688c5ec75f129f
wellborn <143796045+wellborn890@users.noreply.github.com>
2024-04-30 21:13:33 +0100

    Update first.js

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Twitter Follower Counter
+// @name         Twitter Follower Counter 89
 // @namespace    https://github.com/NabiKAZ/Twitter-Follower-Count
 // @version      0.1.382
 // @author       Nabi K.Z. <nabikaz@gmail.com> | www.nabi.ir | @NabiKAZ
