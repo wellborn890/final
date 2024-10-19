f386dcb4e3d850d909f333794915fb62cc7a7016
wellborn <143796045+wellborn890@users.noreply.github.com>
2024-10-19 11:02:22 -0400

    Update first.js

--- first.js
+++ first.js
@@ -6,7 +6,7 @@
 // @match        https://twitter.com/
 // @grant        none of theme
 // ==/UserScript=
-#granted till 26 oct 200i
+#granted till 26 oct 2000
 #failed to continu
 (function() {
     'use strict';
