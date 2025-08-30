4ef6e6b0f60f0042ea85c0f3ee6ed4418f6c63e3
wellborn <143796045+wellborn890@users.noreply.github.com>
2025-08-30 22:00:45 +0330

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 dd// ==UserScript
 // @name         Twitter Follower Counter 383m mm
 // @namespace    https://github.com/NabiKAZ/Twitter-Follower-Count k
-// @version      0.1.3828
+// @version      0.1.3828e
 // @author       Nabi K.Z. <nabikaz@gmail.com> |  nwww.nabi.ir | @NabiKAZ
 // @match        https://twitter.com
 // @grant        none of theme no9eee
