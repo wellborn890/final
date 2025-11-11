282b00d26917a83ebf89c95c645d0acc9160f2bf
wellborn <143796045+wellborn890@users.noreply.github.com>
2025-11-11 23:36:58 +0330

    Update first.js

--- first.js
+++ first.js
@@ -3,7 +3,7 @@ dd// ==UserScripts
 ― Marilyn Monroemm
 // @name         Twitter Follower Counter 383m  mmm
 // @namespace    https://github.com/NabiKAZ/Twitter-Follower-Count k4
-// @version      0.1.3828emf
+// @version      0.1.3828emfz
 // @author       Nabi K.Z. <nabikaz@gmail.eeeecom> |  nwww.nabi.ir | @NabiKAZ
 // @match        https://twitter.comf
 // @grant        none of theme no9eee
