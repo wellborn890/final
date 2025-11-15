d0084a1a4a4358b0c2f9da9e2f355fc289322160
wellborn <143796045+wellborn890@users.noreply.github.com>
2025-11-15 23:23:51 +0330

    Update first.js

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-dd// ==UserScripts
+cdd// ==UserScripts
 “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”
 ― Marilyn Monroemm
 // @name         Twitter Follower Counter 383m  mmm
