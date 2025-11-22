ad7ca907d64382d37b6e5026ebf9b2db10996eea
wellborn <143796045+wellborn890@users.noreply.github.com>
2025-11-22 23:19:03 +0330

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 cdd// ==UserScripts
 “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”
 ― Marilyn Monroemm
-// @name         Twitter Follower Counter 383m  mmm
+// @name         Twitter Follower Counter 383m  mmmk
 // @namespace    https://github.com/NabiKAZ/Twitter-Follower-Count k4xxx
 // @version      0.1.3828emfz
 // @author       Nabi K.Z. <nabikaz@gmail.eeeecom> |  nwww.nabi.ir | @NabiKAZ
